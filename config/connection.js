c433e67a4a2e15afd95e487ad9d06f399eed3819
Vyshnav404 <vyshnav404@gmail.com>
2023-07-18 16:32:03 +0530

    db updated

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
-const MongoClient = require('mongodb').MongoClient
-const state = {
-    db:null
-}
-module.exports.connect = function(done){
-    const url = 'mongodb://127.0.0.1:27017'
-    const dbname = 'Ecommerce'
+const MongoClient = require('mongodb').MongoClient;
 
-    MongoClient.connect(url,(err,data)=>{
-        if(err) return done(err)
-        state.db = data.db(dbname)
-        done()
-    })
-}
-module.exports.get = function(){
-    return state.db
-}
+const state = {
+  db: null
+};
+
+module.exports.connect = function(done) {
+  const url = 'mongodb://127.0.0.1:27017';
+  const dbname = 'Ecommerce';
+
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+      return done(err);
+    }
+    state.db = client.db(dbname);
+    done();
+  });
+};
+
+module.exports.get = function() {
+  return state.db;
+};
