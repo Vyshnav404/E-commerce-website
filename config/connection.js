ba237041c5e17a283a6dc28e44146468c4cf2f0f
Vyshnav404 <vyshnav404@gmail.com>
2023-07-18 17:10:04 +0530

    db updated again

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const state = {
 };
 
 module.exports.connect = function(done) {
-  const url = 'mongodb://127.0.0.1:0.0.0.0';
+  const url = 'mongodb://0.0.0.0:27017';
   const dbname = 'Ecommerce';
 
   MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
