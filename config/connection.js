67fce6654c7226811f49773d099b2d6499a79765
Vyshnav404 <vyshnav404@gmail.com>
2023-07-20 16:23:42 +0530

    database updated

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,5 @@
+const { error } = require('console');
+
 const MongoClient = require('mongodb').MongoClient;
 
 const state = {
@@ -8,13 +10,20 @@ module.exports.connect = function(done) {
   const url = 'mongodb+srv://vyshnav404:cxpqwhpRjbDb8rBT@woodq-ecommerce.3mxv8uj.mongodb.net/Ecommerce';
   const dbname = 'Ecommerce';
 
-  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-    if (err) {
-      return done(err);
-    }
-    state.db = client.db(dbname);
-    done();
-  });
+
+  try {
+    
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+      console.log("mongoe ===",err);
+      if (err) {
+        return done(err);
+      }
+      state.db = client.db(dbname);
+      done();
+    });
+  } catch (error) {
+    console.log("what is mongo error==",error);
+  }
 };
 
 module.exports.get = function() {
