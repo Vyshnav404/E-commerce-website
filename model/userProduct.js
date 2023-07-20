947199f8a6ecd641004d9445375cf0f6d3db89e1
Vyshnav404 <vyshnav404@gmail.com>
2023-07-20 16:28:45 +0530

    database collection updated

--- model/userProduct.js
+++ model/userProduct.js
@@ -1,18 +1,18 @@
 const db = require('../config/connection');
-const collection = require('../config/collection');
+const collections = require('../config/collection');
 const { ObjectId } = require('mongodb');
 
 
 module.exports = {
     displayProduct:()=>{
         return new Promise(async(resolve,reject)=>{
-            let productDetails = await db.get().collection(collection?.Product_Details).find().toArray()
+            let productDetails = await db.get().collection(collections?.Product_Details).find().toArray()
             resolve(productDetails)
         })
     },
     viewCatBase :(catname)=>{
         return new Promise(async(resolve,reject)=>{
-          let catProducts= await db.get().collection(collection.Product_Details).aggregate([{
+          let catProducts= await db.get().collection(collections.Product_Details).aggregate([{
             $match:{categoryName:catname}
           }]).toArray()
           resolve(catProducts)
@@ -21,7 +21,7 @@ module.exports = {
 
     viewProductDetails:(productId)=>{
       return new Promise((resolve,reject)=>{
-        db.get().collection(collection.Product_Details).findOne({_id:ObjectId(productId)}).then((product)=>{
+        db.get().collection(collections.Product_Details).findOne({_id:ObjectId(productId)}).then((product)=>{
           resolve(product)
         })
       })
